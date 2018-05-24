a144c94d7bc50337990df3a9c571851c865eebbb
pubkey <daniel_van_meyer@hotmail.de>
2018-05-24 18:45:40 +0200

    FIX cleanup after test

--- test-cli.bash
+++ test-cli.bash
@@ -13,3 +13,6 @@ rm -rf ./test/contracts/Basic.js
 rm -rf ./test/contracts/Basic.ts
 node ./dist/src/cli.js -i ./test/contracts/Basic.sol
 node ./test/contracts/Basic.js # test if valid
+
+
+rm -rf solidity-cli
